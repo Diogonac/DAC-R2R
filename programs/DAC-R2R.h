539e9b6137ac0fb3e44e9aaccba0f3ee94433819
Diogonac <diogonac@al.insper.edu.br>
2022-01-14 11:42:26 -0300

    update

--- programs/DAC-R2R.h
+++ programs/DAC-R2R.h
@@ -23,16 +23,6 @@ bool aux_deb;
 int i, cont, j;
 double tempo, espera, start, initialization;
 
-double freq[60] = {5,   10,  15,  20,  25,  30,  35,  40,  45,  50,  55,  60,
-                   65,  70,  75,  80,  85,  90,  95,  100, 105, 110, 115, 120,
-                   125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180,
-                   185, 190, 195, 200, 205, 210, 215, 220, 225, 230, 235, 240,
-                   245, 250, 255, 260, 265, 270, 275, 280, 285, 290, 295, 300};
-
-int periodos[60] = {
-
-};
-
 char underline[8] =
 {
 0b00000,
@@ -82,40 +72,36 @@ void rotina_frequencia(int pulse); // Rotina para calcular a frequência dado o
     
     }
 
-    switch (cont) {
+    // switch (cont) {
 
-    case 1:
-        wave_init();
-        break;
+    // case 1:
+    //     wave_init();
+    //     break;
     
-    case 2:
-        LCD.cls();
-        cont = 3;
-        break;
+    // case 2:
+    //     LCD.cls();
+    //     cont = 3;
+    //     break;
     
-    case 3:
-        wave_selection_1();
-        // cont = 4;
-        break;
+    // case 3:
+    //     wave_selection_1();
+    //     // cont = 4;
+    //     break;
     
-    case 4:
-        slider();
-        break;
+    // case 4:
+    //     slider();
+    //     break;
 
+    // }
 
-
-
-
-    }
-
-    // if (cont == 0)
-    //   rotina_rampa();
-    // if (cont == 1)
-    //   rotina_triangular();
-    // if (cont == 2)
-    //   rotina_senoidal();
-    // if (cont >= 3)
-    //   cont = 0;
+    if (cont == 0)
+      rotina_rampa();
+    if (cont == 1)
+      rotina_triangular();
+    if (cont == 2)
+      rotina_senoidal();
+    if (cont >= 3)
+      cont = 0;
   }
   
 
@@ -246,5 +232,5 @@ void rotina_frequencia(int pulse) {
 
 //     espera = ((1 / (freq[0] * 256)));
 //   }
-printf("Pulsos: %d\r\n", pulse);
+//printf("Pulsos: %d\r\n", pulse);
 }
